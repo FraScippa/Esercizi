7d3d6f1aa90e5d4d5bd99655b1940f7ae031e09b
FraScippa <francy.scippa@gmail.com>
2024-04-23 11:13:33 +0200

    risolto4-2

--- Lezione3/Lezione3.py
+++ Lezione3/Lezione3.py
@@ -11,4 +11,21 @@ for pizza in pizzas:
     #print(pizza) for print only pizzas.
     print(f'I like so much the {pizza}. I really love pizza!')
 
+#4-2. Animals: Think of at least three different animals that have a common characteristic. 
+#Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
+#• Modify your program to print a statement about each animal, such as A dog would make a great pet.
+#• Add a line at the end of your program, stating what these animals have in common. You could print a sentence, 
+#such as Any of these animals would make a great pet!
+
+animals: list = ['dog','cat','horse']
+
+for animal in animals:
+    #print(animal)
+    print(f"{animal} is a great pet")
+print('Any of these animals would make a great pet!')
+
+
+
+
+
 
