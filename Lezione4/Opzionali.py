b1d816cdea8b11960ecfb07ed70baf54a4239171
Francesca <francy.scippa@gmail.com>
2024-05-01 22:51:02 +0200

    risolved1-2-3

--- Lezione4/Opzionali.py
+++ Lezione4/Opzionali.py
@@ -89,32 +89,121 @@ def guess_the_number(start: int, stop: int) -> None:
 #The function should calculate the cart total and apply any discounts or taxes.
 #Implement a for loop to iterate over the items in the cart and print detailed information about each product and the total.
 
-                 
-            
+def products() -> None:
+    """Print all of products"""
     
+    print("### Welcome, to our online store! ####\nThese are our products.")
     
+    product: dict= {'T-shirt':{"product":'T-shirt',"price": "19.99€", "quantity": 1},
+    'Skirt':{"product":'Skirt',"price":"35.50€", "quantity": 1},
+    'Shoes':{"product":'Shoes',"price":"80.00€", "quantity": 1},
+    'Trouses':{"product":'Trouses',"price":"45.20€", "quantity": 1},
+    'Socks':{"product":'Socks',"price":"9.99€", "quantity": 1},
+    'Accessories':{"product":'Accessories',"price":"4.50€", "quantity": 1},
+    'Belt':{"product":'Belt',"price":"25.00€", "quantity": 1}}
+    
+    for item in product.values():
+        print(item)
+    
+    print("### Happy Shopping! ###")
 
+def add_cart(cart: list, product: str) -> None:
+    """Add the product in the cart."""
+    cart.append(product) 
+    
+def remove_cart(cart:list , product_name: str) -> None:
+    """Revome the product to the cart."""
+    for item in cart:
+        if item['product'] == product_name:
+            cart.remove(item)
+            break  
 
+def view_cart(cart: list) -> None:
+    """Shows you what is in the cart."""
+    if cart:
+        print("You shopping cart: ")
+        for item in cart:
+            print(f"product {item['product']}, price: {item['price']}€, quantity: {item['quantity']}") 
+    else:
+        print("Your cart is empty.")
+        
+def calculate_tot(cart: list) -> None:
+    """Calculate the total of the cart."""
+    total: float = sum(item['price'] * item['quantity'] for item in cart)
+    return total
 
+product: dict= {'T-shirt':{"product":'T-shirt',"price": 19.99, "quantity": 1},
+    'Skirt':{"product":'Skirt',"price": 35.50, "quantity": 1},
+    'Shoes':{"product":'Shoes',"price": 80.00, "quantity": 1},
+    'Trouses':{"product":'Trouses',"price": 45.20, "quantity": 1},
+    'Socks':{"product":'Socks',"price": 9.99, "quantity": 1},
+    'Accessories':{"product":'Accessories',"price": 4.50, "quantity": 1},
+    'Belt':{"product":'Belt',"price": 25.00, "quantity": 1}}
 
-    
-    
-    
-    
-    
-    
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    
-    
-       
-    
-    
+cart: list = []
 
+while True:
+    
+    products()
+    
+    name: str = input("Enter the product you want to buy: ")
+    quantity: int = int(input("How many do you want?(Type a number 1 from 10): "))
+    
+    if name in product.keys():
+        price = product[name]["price"]
+        object = {"product": name, "price": price, "quantity": quantity}
+        add_cart(cart, object)
+    else:
+        print("Product not found!")
+        
+    message: str = input("Do you wanto to continue your shopping? (y/n): ").lower()
+    if message != "y":
+        break
+    
+    view_cart(cart)
+    tot: float = calculate_tot(cart)
+    print(f"Total: {tot}€")
+    
+    message_remove: str = input("Do you want to remove any product from your cart? (y/n): ").lower()
+    if message_remove == 'y':
+        product_name: str = input("Enter the product you want to remove: ")
+        remove_cart(cart, product_name)
+        view_cart(cart)
+        tot: float = calculate_tot(cart)
+        print(f"Total: {tot}€")
+    
+#5. Inventory Management System:
+#Create a function that defines an item with a code, name, quantity, and price.
+#Create a database or dictionary to store the items in inventory.
+#Implement functions to add, remove, search, and update items in the inventory.
+#Use for loops and conditional statements to manage the various inventory operations.
+
+def item(code: int, name: str, quantity: int, price: float) -> dict:
+    """Defines an item, with the paraments request."""
+    items: dict = {'code': code, 'name': name, 'quantity': quantity, 'price': price}
+    return items
+    
+def add_items(inventory: dict, item: dict) -> None:
+    """Add a new item in the dictionary."""
+    code = item['code']
+    if code not in inventory:
+        inventory[code] = item
+    
+def remove_item(inventory: dict, item_code: str) -> None:
+    """Remove an item in the dictionary."""
+    if item_code in inventory:
+        inventory.pop(item_code)
+    else:
+        print("The item with this code dosen't exist in the inventory.")
+
+def update_item(inventory: dict, item_code) -> None:
+    """If the item already exists increase its quantity by 1."""
+    if item_code in inventory:
+        inventory[item_code]['quantitaty'] += 1
+    else:
+        print("The item with this code dosen't exist in the inventory.")
+
+inventory: dict = {} 
+
+while True:
+    
