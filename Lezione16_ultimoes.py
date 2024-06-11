6a16ab315069840309f6eff649c98d5a39220d0b
FraScippa <francy.scippa@gmail.com>
2024-06-11 19:23:15 +0200

    fiisici

--- Lezione16_ultimoes.py
+++ Lezione16_ultimoes.py
@@ -1,36 +1,32 @@
 class Specie:
     
     def __init__(self,popolazione_iniziale: int, tasso_crescita: float):
-        self.popolazione_iniziale: int = popolazione_iniziale
+        self.popolazione_iniziale: float = popolazione_iniziale
         self.tasso_crescita: float = tasso_crescita
-    
-    def set_nome(self, nome: str):
-        self.nome = nome
-        return self.nome
 
-    
     def cresci(self) -> float:
         popolazione_nuova: float = self.popolazione_iniziale*(1 + self.tasso_crescita/100)
-        return popolazione_nuova
+        self.popolazione_iniziale = popolazione_nuova
+        return self.popolazione_iniziale
     
     def getDensita(self, area_kmq: float):
         densita: float = self.popolazione_iniziale/area_kmq
-        
-        while densita <= 1: 
+        anni: int = 0
+        while densita < 1.0: 
             self.cresci()
+            anni += 1
+        return anni
                 
-            
     def anni_per_superare(self, altra_specie: 'Specie'):
         anni: int = 0
         
-        while anni < 1000:
+        while True:
+            self.cresci()
+            altra_specie.cresci()
+            anni += 1
             if self.popolazione_iniziale < altra_specie.popolazione_iniziale:
-                self.cresci()
-                altra_specie.cresci()
-                anni += 1
-        
-        return anni
-       
+                return anni
+                
 class BufaloKlingon(Specie):
     
     def __init__(self, popolazione_iniziale: int, tasso_crescita: float):
@@ -44,6 +40,9 @@ class Elefante(Specie):
 
 bufalo_klingon = BufaloKlingon(100, 15)  
 elefante = Elefante(10, 35)
-
+print(elefante.cresci())
+print(bufalo_klingon.cresci())
+anni_densita = bufalo_klingon.getDensita(1500)
+print(f"Anni necessari per raggiungere una densità di 1 Bufalo Klingon per km quadrato: {anni_densita}")
 anni_necessari = elefante.anni_per_superare(bufalo_klingon)  
 print(f"Anni necessari perché la popolazione di elefanti superi quella dei bufali Klingon: {anni_necessari}")
