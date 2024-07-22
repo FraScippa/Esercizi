bee5f3855b0dd627a55d316891829ec87088668c
Francesca <francy.scippa@gmail.com>
2024-07-22 21:58:50 +0200

    .

--- Lezione24/Lezione24.py
+++ Lezione24/Lezione24.py
@@ -1,48 +1,40 @@
 from abc import ABC, abstractmethod
-import string
 
 class CodificatoreMessaggio(ABC):
     
     @abstractmethod
-    def codifica_signolo(self, car: str):
-        #import string
-        #alfabeto: str = string.ascii_lowercase 
-        alfabeto: list[str] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ']
-        if car.lower() in alfabeto:
-            index = alfabeto.index(car.lower())
-            return index 
-    
-    @abstractmethod
-    def codifica(self, mess_codificare: str)-> int:
-        s = ''
-        for c in mess_codificare:
-            mess = str(self.codifica_signolo(c))
-            s += f' {mess}'
-        return s
+    def codifica(self, mess_codificare: str)-> str:
+        pass
 
 class DecodificatoreMessaggio(ABC):
     
-    def decodifica(self, testo_codificato: str):
+    def decodifica(self, testo_codificato: list[int]) -> str:
         alfabeto: list[str] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        dec: str = ''
         for c in testo_codificato:
-            mess = testo_codificato.strip()
-        
-        return alfabeto[mess]
+            dec += alfabeto[c]
+        return dec
             
-        
-        
-        
-    
 class CifratoreAScorrimento(CodificatoreMessaggio, DecodificatoreMessaggio):
     
     def __init__(self, chiave: int):
         self.chiave: int = chiave
     
-    def sposta_carattere(self, car: str):
-        alfabeto: str = string.ascii_lowercase + string.ascii_uppercase
-        for i in alfabeto:
-            if car in alfabeto:
-                return i
+    def __sposta_carattere(self, car: str): 
+        #import string
+        #alfabeto: str = string.ascii_lowercase 
+        alfabeto: list[str] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        if car.lower() in alfabeto:
+            index = alfabeto.index(car.lower())
+            return alfabeto[index + self.chiave]
+        else:
+            return car.lower()
+        
+    def codifica(self, mess_codificare: str) -> str:
+        s = ''
+        for c in mess_codificare:
+            mess = str(self.__sposta_carattere(c))
+            s += f' {mess}'
+        return s
             
-            
-print()
+    
--- Lezione24/test.py
+++ Lezione24/test.py
@@ -1,31 +1,6 @@
+from Lezione24 import CifratoreAScorrimento
 
+chiave1: CifratoreAScorrimento = CifratoreAScorrimento(3)
 
-def codifica_signolo(car: str):
-    #import string
-    #alfabeto: str = string.ascii_lowercase 
-    alfabeto: list[str] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    if car.lower() in alfabeto:
-        index = alfabeto.index(car.lower())
-        return index
-    
-print(codifica_signolo("o"))
-
-def codifica(mess_codificare: str):
-    s = ''
-    for c in mess_codificare:
-        mess = str(codifica_signolo(c))
-        s += f' {mess}'
-    return s
-
-def decodifica(testo_codificato: str):
-    alfabeto: list[str] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    dec: list[int] = []
-    for c in testo_codificato:
-        mess = int(testo_codificato.strip())
-        dec.append(mess)
-        
-    
-    return dec
-
-print(codifica("Bye"))
-print(decodifica("1244"))
+print(chiave1.codifica("can"))
+print(chiave1.codifica("bye"))
